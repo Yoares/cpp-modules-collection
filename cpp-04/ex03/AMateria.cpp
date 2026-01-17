07fa3f51d5659c1be682073c9ff361ec1a5fa282
Yasser Khoussi <ykhoussi@c4r7p2.1337.ma>
2026-01-17 21:38:41 +0100

    Ice class

--- cpp-04/ex03/AMateria.cpp
+++ cpp-04/ex03/AMateria.cpp
@@ -1,1 +1,32 @@
 #include "AMateria.hpp"
+
+AMateria::AMateria(){
+    this->type = "default";
+    std::cout << "AMateria default constructor called" << std::endl;
+}
+
+AMateria::AMateria(const std::string& type){
+    this->type = type;
+    std::cout << "AMateria parameterized constructor called" << std::endl;
+}
+
+AMateria::AMateria(const AMateria& other){
+    this->type = other.type; 
+    std::cout << "AMateria copy constructor called" << std::endl;
+}
+
+AMateria& AMateria::operator=(const AMateria& other){
+    if (this != &other) {
+        this->type = other.type;
+    }
+    std::cout << "AMateria assignment operator called" << std::endl;
+    return *this;
+}
+
+AMateria::~AMateria(){
+    std::cout << "AMateria destructor called" << std::endl;
+}
+
+const std::string& AMateria::getType() const{
+    return this->type;
+}
--- cpp-04/ex03/AMateria.hpp
+++ cpp-04/ex03/AMateria.hpp
@@ -6,45 +6,22 @@
 
 class AMateria
 {
-protected:
-    std::string type;
-    // Stores the Materia type ("ice", "cure", etc.)
-    // Protected so derived classes can set it
+    protected:
+        std::string type;
+        // Stores the Materia type ("ice", "cure", etc.)
 
-public:
-    AMateria();
-    // Default constructor
-    // Initializes the base part of a Materia
-
-    AMateria(const std::string& type);
-    // Constructor that sets the Materia type
-    // Used by derived classes
-
-    AMateria(const AMateria& other);
-    // Copy constructor
-    // Copies only base-level data (type)
-
-    AMateria& operator=(const AMateria& other);
-    // Assignment operator
-    // Copies only base-level data (type)
-
-    virtual ~AMateria();
-    // MUST be virtual
-    // Allows safe deletion via AMateria*
-
-    const std::string& getType() const;
-    // Returns the Materia type
-    // Does NOT expose ownership
-
-    virtual AMateria* clone() const = 0;
-    // PURE VIRTUAL
-    // Forces derived classes to implement deep copy
-    // Returns a NEW object allocated on the heap
-
-    // virtual void use(ICharacter& target);
-    // Virtual (NOT pure)
-    // Default behavior may exist
-    // Can be overridden by derived classes
+    public:
+        AMateria();
+        AMateria(const std::string& type);
+        AMateria(const AMateria& other);
+        AMateria& operator=(const AMateria& other);
+        virtual ~AMateria();
+        const std::string& getType() const;
+        virtual AMateria* clone() const = 0;
+        // virtual void use(ICharacter& target);
+        // Virtual (NOT pure)
+        // Default behavior may exist
+        // Can be overridden by derived classes
 };
 
 #endif // AMATERIA_HPP
